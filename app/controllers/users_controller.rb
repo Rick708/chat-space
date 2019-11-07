373e4f9a3a9fabbd11e3ab264397ef3f0ec1e1f7
Rick708 <Rick708@users.noreply.github.com>
2019-11-08 02:02:34 +0900

    コンフリクト修正しました

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
 
+<<<<<<< HEAD
   def edit
   end
 
@@ -16,4 +17,23 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :email)
   end
+=======
+    def edit
+    end
+  
+    def update
+      if current_user.update(user_params)
+        redirect_to root_path
+      else
+        render :edit
+      end
+    end
+  
+    private
+  
+    def user_params
+      params.require(:user).permit(:name, :email)
+    end
+
+>>>>>>> master
 end
